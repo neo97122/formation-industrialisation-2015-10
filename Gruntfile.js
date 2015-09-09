6cceeee896a1f92b7592e8109aec9770068cc206
bioub <romain.bohdanowicz@gmail.com>
2015-09-09 12:34:45 +0100

    Ajout d'une tache less-autoprefix

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+    'use strict';
+
+    require('jit-grunt')(grunt);
+
+    grunt.initConfig({
+        less: {
+            dev: {
+                files: [{
+                    expand: true,
+                    cwd: 'less',
+                    src: ['*.less'],
+                    dest: 'css/',
+                    ext: '.css'
+                }]
+            }
+        },
+        autoprefixer: {
+            options: {
+              browsers: ['last 3 versions', 'ie 8', 'ie 9']
+            },
+            dev: {
+                files: [{
+                    expand: true,
+                    src: ['css/*.css'],
+                }]
+            }
+        }
+    });
+
+    grunt.registerTask('less-autoprefix', [
+        'less:dev',
+        'autoprefixer:dev'
+    ])
+};
